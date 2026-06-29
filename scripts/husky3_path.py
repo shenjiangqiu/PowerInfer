e18115d4a7ca0364330f557b3d079cea661811e8
Jiangqiu Shen <ivy22233qiu@live.com>
2026-06-29 19:43:30 +0000

    update path

---
+++ scripts/husky3_path.py
@@ -0,0 +1,9 @@
+MODEL_MAP = {
+    "ReluLLaMA-7B": (
+        "/home/sjq/sjq10t/hfhome/hub/models--PowerInfer--ReluLLaMA-7B-PowerInfer-GGUF/snapshots/17b5d8a28f0377e05758a74989ce9326f5905860/"
+        + "llama-7b-relu.powerinfer.gguf"
+    ),
+    "Bamboo-7B": "/home/sjq/sjq10t/hfhome/hub/models--PowerInfer--Bamboo-base-v0.1-gguf/snapshots/da6530bc54af41183383df7923701f8c6333b3e7/bamboo-7b-v0.1.powerinfer.gguf",
+    "Bamboo-dpo-7B": "/home/sjq/sjq10t/hfhome/hub/models--PowerInfer--Bamboo-DPO-v0.1-gguf/snapshots/c3847e080bd8664f2ea299400f74665ca5b13824/bamboo-7b-dpo-v0.1.powerinfer.gguf",
+    "ProSparse-llama-7b": "/home/sjq/sjq10t/hfhome/hub/models--PowerInfer--ProSparse-LLaMA-2-7B-GGUF/snapshots/72f4a41882ede0cbc745150559b4db268a142b36/prosparse-llama-2-7b-clip15.gguf",
+}
--- scripts/run_sparse_dump_bin.py
+++ scripts/run_sparse_dump_bin.py
@@ -13,6 +13,7 @@ import os
 import subprocess
 import sys
 from pathlib import Path
+from .husky3_path import MODEL_MAP
 
 SCRIPT_DIR = Path(__file__).resolve().parent
 POWERINFER_DIR = SCRIPT_DIR.parent
@@ -25,15 +26,7 @@ from download_dataset import (
     download_chatgpt,
 )
 
-MODEL_MAP = {
-    "ReluLLaMA-7B": (
-        "/home/sjq/sjq10t/hfhome/hub/models--PowerInfer--ReluLLaMA-7B-PowerInfer-GGUF/snapshots/17b5d8a28f0377e05758a74989ce9326f5905860/"
-        + "llama-7b-relu.powerinfer.gguf"
-    ),
-    "Bamboo-7B": "/home/sjq/sjq10t/hfhome/hub/models--PowerInfer--Bamboo-base-v0.1-gguf/snapshots/da6530bc54af41183383df7923701f8c6333b3e7/bamboo-7b-v0.1.powerinfer.gguf",
-    "Bamboo-dpo-7B": "/home/sjq/sjq10t/hfhome/hub/models--PowerInfer--Bamboo-DPO-v0.1-gguf/snapshots/c3847e080bd8664f2ea299400f74665ca5b13824/bamboo-7b-dpo-v0.1.powerinfer.gguf",
-    "ProSparse-llama-7b": "/home/sjq/sjq10t/hfhome/hub/models--PowerInfer--ProSparse-LLaMA-2-7B-GGUF/snapshots/72f4a41882ede0cbc745150559b4db268a142b36/prosparse-llama-2-7b-clip15.gguf",
-}
+
 
 DATASET_HANDLERS = {
     "wiki": download_wiki,
